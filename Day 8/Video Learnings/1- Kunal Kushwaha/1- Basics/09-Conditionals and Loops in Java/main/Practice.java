b87203d38c440dcc537d82d16764cfda32179293
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-09-20 01:21:22 +0500

    Day 8

---
+++ Day 8/Video Learnings/1- Kunal Kushwaha/1- Basics/09-Conditionals and Loops in Java/main/Practice.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        String str = " abd ";
+        str = str.trim();
+        System.out.println(str.charAt(str.length()-1));
+    }
+}
