9918c20398c58397eb5fa3bf350ebbf3462c6e25
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-09-28 01:14:13 +0500

    Day 12

---
+++ Day 12/Problems/1- Kunal Kushwaha/1- Array/Extra Problems/Extra_Problem_1_Print_all_subarrays.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Extra_Problem_1_Print_all_subarrays{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        List<List<Integer>> arraylist = new ArrayList<>();
+        arraylist = findAllSubarrays(arr);
+        for(List<Integer> myList : arraylist){
+            System.out.println(myList);
+        }
+    }
+    public static List<List<Integer>> findAllSubarrays(int[] arr){
+        List<List<Integer>> arraylist = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            List<Integer> myList = new ArrayList<>();
+            for(int j=i; j<arr.length; j++){
+                myList.add(arr[j]);
+                arraylist.add(new ArrayList<>(myList));
+            }
+        }
+
+        return arraylist;
+    }
+}
