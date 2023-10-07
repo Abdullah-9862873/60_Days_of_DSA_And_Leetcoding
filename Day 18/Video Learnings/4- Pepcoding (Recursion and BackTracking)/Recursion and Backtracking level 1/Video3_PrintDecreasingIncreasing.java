9e9158ff32b43de38c182d11b72b3e3de6100386
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-07 23:20:20 +0500

    Day 18

---
+++ Day 18/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 1/Video3_PrintDecreasingIncreasing.java
@@ -0,0 +1,13 @@
+public class Video3_PrintDecreasingIncreasing {
+    public static void main(String[] args) {
+        printDecInc(5);
+    }
+    public static void printDecInc(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);      
+        printDecInc(n-1);  
+        System.out.println(n);
+    }
+}
