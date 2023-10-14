6ae2284a87808216a37e2b75b8ab6f054823b472
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-14 15:52:05 +0500

    Update Video_10_Minimum_Path_Sum_in_Grid.java

--- Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/02_DP on Grids 2D array/Video_10_Minimum_Path_Sum_in_Grid.java
+++ Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/02_DP on Grids 2D array/Video_10_Minimum_Path_Sum_in_Grid.java
@@ -26,101 +26,101 @@ https://www.codingninjas.com/studio/problems/minimum-path-sum_985349?source=yout
 */
 //_____________________________________________________________
 // Using Recursion
-// public class Video_10_Minimum_Path_Sum_in_Grid {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//             {5,9,6},
-//             {11,5,2}
-//         };
-//         int ans = getCostOfAllPaths(arr, 0,0);
-//         System.out.println(ans);
-//     }
-//     public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart){
-//         if(rowStart == arr.length-1 && colStart == arr[0].length-1){
-//             return arr[rowStart][colStart];
-//         }
+public class Video_10_Minimum_Path_Sum_in_Grid {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {5,9,6},
+            {11,5,2}
+        };
+        int ans = getCostOfAllPaths(arr, 0,0);
+        System.out.println(ans);
+    }
+    public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart){
+        if(rowStart == arr.length-1 && colStart == arr[0].length-1){
+            return arr[rowStart][colStart];
+        }
 
-//         int firstAnswer = Integer.MAX_VALUE;
-//         int secondAnswer = Integer.MAX_VALUE;
-//         if(rowStart < arr.length-1){
-//             firstAnswer = arr[rowStart][colStart] +getCostOfAllPaths(arr, rowStart+1, colStart);
-//         }
-//         if(colStart < arr[0].length-1){
-//             secondAnswer = arr[rowStart][colStart] + getCostOfAllPaths(arr, rowStart, colStart+1);
-//         }
-//         return Math.min(firstAnswer, secondAnswer);
-//     }
-// }
+        int firstAnswer = Integer.MAX_VALUE;
+        int secondAnswer = Integer.MAX_VALUE;
+        if(rowStart < arr.length-1){
+            firstAnswer = arr[rowStart][colStart] +getCostOfAllPaths(arr, rowStart+1, colStart);
+        }
+        if(colStart < arr[0].length-1){
+            secondAnswer = arr[rowStart][colStart] + getCostOfAllPaths(arr, rowStart, colStart+1);
+        }
+        return Math.min(firstAnswer, secondAnswer);
+    }
+}
 //_____________________________________________________________
 // Memoization
-// import java.util.*;
-// public class Video_10_Minimum_Path_Sum_in_Grid {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//             {5,9,6},
-//             {11,5,2}
-//         };
-//         int[][] dp = new int[arr.length][arr[0].length];
-//         for(int[] temp: dp){
-//             Arrays.fill(temp, -1);
-//         }
-//         int ans = getCostOfAllPaths(arr, 0,0, dp);
-//         System.out.println(ans);
-//     }
-//     public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart, int[][] dp){
-//         if(rowStart == arr.length-1 && colStart == arr[0].length-1){
-//              return dp[rowStart][colStart] = arr[rowStart][colStart];
-//         }
-//         if(dp[rowStart][colStart] != -1){
-//             return dp[rowStart][colStart];
-//         }
+import java.util.*;
+public class Video_10_Minimum_Path_Sum_in_Grid {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {5,9,6},
+            {11,5,2}
+        };
+        int[][] dp = new int[arr.length][arr[0].length];
+        for(int[] temp: dp){
+            Arrays.fill(temp, -1);
+        }
+        int ans = getCostOfAllPaths(arr, 0,0, dp);
+        System.out.println(ans);
+    }
+    public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart, int[][] dp){
+        if(rowStart == arr.length-1 && colStart == arr[0].length-1){
+             return dp[rowStart][colStart] = arr[rowStart][colStart];
+        }
+        if(dp[rowStart][colStart] != -1){
+            return dp[rowStart][colStart];
+        }
 
-//         int firstAnswer = Integer.MAX_VALUE;
-//         int secondAnswer = Integer.MAX_VALUE;
-//         if(rowStart < arr.length-1){
-//             firstAnswer = arr[rowStart][colStart] +getCostOfAllPaths(arr, rowStart+1, colStart, dp);
-//         }
-//         if(colStart < arr[0].length-1){
-//             secondAnswer = arr[rowStart][colStart] + getCostOfAllPaths(arr, rowStart, colStart+1, dp);
-//         }
-//         return dp[rowStart][colStart] = Math.min(firstAnswer, secondAnswer);
-//     }
-// }
+        int firstAnswer = Integer.MAX_VALUE;
+        int secondAnswer = Integer.MAX_VALUE;
+        if(rowStart < arr.length-1){
+            firstAnswer = arr[rowStart][colStart] +getCostOfAllPaths(arr, rowStart+1, colStart, dp);
+        }
+        if(colStart < arr[0].length-1){
+            secondAnswer = arr[rowStart][colStart] + getCostOfAllPaths(arr, rowStart, colStart+1, dp);
+        }
+        return dp[rowStart][colStart] = Math.min(firstAnswer, secondAnswer);
+    }
+}
 
 //_____________________________________________________________
 // Using Tabulation
-// public class Video_10_Minimum_Path_Sum_in_Grid {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//             {5,9,6},
-//             {11,5,2}
-//         };
-//         int ans = getCostOfAllPaths(arr, 0,0);
-//         System.out.println(ans);
-//     }
-//     public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart){
-//         int[][] dp = new int[arr.length][arr[0].length];
-//         dp[arr.length-1][arr[0].length-1] = arr[arr.length-1][arr[0].length-1];
+public class Video_10_Minimum_Path_Sum_in_Grid {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {5,9,6},
+            {11,5,2}
+        };
+        int ans = getCostOfAllPaths(arr, 0,0);
+        System.out.println(ans);
+    }
+    public static int getCostOfAllPaths(int[][] arr, int rowStart, int colStart){
+        int[][] dp = new int[arr.length][arr[0].length];
+        dp[arr.length-1][arr[0].length-1] = arr[arr.length-1][arr[0].length-1];
 
-//         for(int i=arr.length-1; i>=0; i--){
-//             for(int j=arr[0].length-1; j>=0; j--){
-//                 if(i==arr.length-1 && j==arr[0].length-1){
-//                     continue;
-//                 }
-//                 else if(i == arr.length-1 && j < arr[0].length-1){
-//                     dp[i][j] = arr[i][j] + dp[i][j+1];
-//                 }else if( i < arr.length-1 && j == arr.length-1){
-//                     dp[i][j] = arr[i][j] + dp[i+1][j];
-//                 }else if(i < arr.length-1 && j < arr.length-1){
-//                     int ans1 = arr[i][j] + dp[i+1][j];
-//                     int ans2 = arr[i][j] + dp[i][j+1];
-//                     dp[i][j] = Math.min(ans1, ans2);
-//                 }
-//             }
-//         }
-//         return dp[0][0];
-//     }
-// }
+        for(int i=arr.length-1; i>=0; i--){
+            for(int j=arr[0].length-1; j>=0; j--){
+                if(i==arr.length-1 && j==arr[0].length-1){
+                    continue;
+                }
+                else if(i == arr.length-1 && j < arr[0].length-1){
+                    dp[i][j] = arr[i][j] + dp[i][j+1];
+                }else if( i < arr.length-1 && j == arr.length-1){
+                    dp[i][j] = arr[i][j] + dp[i+1][j];
+                }else if(i < arr.length-1 && j < arr.length-1){
+                    int ans1 = arr[i][j] + dp[i+1][j];
+                    int ans2 = arr[i][j] + dp[i][j+1];
+                    dp[i][j] = Math.min(ans1, ans2);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
 
 //_____________________________________________________________
 // Space Optimization
