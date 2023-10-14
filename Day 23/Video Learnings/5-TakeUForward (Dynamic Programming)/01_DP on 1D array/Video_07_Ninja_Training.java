dd8097c7950960818b0ea26206584582a9122cf6
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-14 15:56:25 +0500

    Update Video_07_Ninja_Training.java

--- Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/01_DP on 1D array/Video_07_Ninja_Training.java
+++ Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/01_DP on 1D array/Video_07_Ninja_Training.java
@@ -29,170 +29,172 @@ https://www.codingninjas.com/studio/problems/ninja-s-training_3621003?source=you
 
 */
 //______________________________________________________________________
-// public class Video_07_Ninja_Training {
-//     public static void main(String[] args) {
-// int[][] arr = {
-//     {10,50,1},
-//     {5,100,11},
-//     {1,5,6},
-//     {2,12,24}
-// };
-//         boolean[] availability = new boolean[arr[0].length];
+public class Video_07_Ninja_Training {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 50, 1 },
+                { 5, 100, 11 },
+                { 1, 5, 6 },
+                { 2, 12, 24 }
+        };
+        boolean[] availability = new boolean[arr[0].length];
 
-//         printAllCombinations(arr, availability, 0, "");
-//     }
-//     public static void printAllCombinations(int[][] arr, boolean[] availability, int days, String asf){
-//         if(days >= arr.length){
-//             if(days == arr.length){
-//                 System.out.println(asf);
-//             }
-//             return;
-//         }
+        printAllCombinations(arr, availability, 0, "");
+    }
 
-//         for(int i=0; i<arr[0].length; i++){
-//             if(availability[i] == false){
-//                 boolean[] newAvailability = new boolean[availability.length];
-//                 newAvailability[i] = true;
-//                 printAllCombinations(arr, newAvailability, days+1, asf.isEmpty() ? asf + arr[days][i] : asf + "-" + arr[days][i]);
-//                 newAvailability[i] = false;
-//             }
-//         }
-//     }
-// }
+    public static void printAllCombinations(int[][] arr, boolean[] availability, int days, String asf) {
+        if (days >= arr.length) {
+            if (days == arr.length) {
+                System.out.println(asf);
+            }
+            return;
+        }
+
+        for (int i = 0; i < arr[0].length; i++) {
+            if (availability[i] == false) {
+                boolean[] newAvailability = new boolean[availability.length];
+                newAvailability[i] = true;
+                printAllCombinations(arr, newAvailability, days + 1,
+                        asf.isEmpty() ? asf + arr[days][i] : asf + "-" + arr[days][i]);
+                newAvailability[i] = false;
+            }
+        }
+    }
+}
 //______________________________________________________________________
 // Other Recursion Method
 
-// public class Video_07_Ninja_Training {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//                 { 10, 50, 1 },
-//                 { 5, 100, 11 },
-//                 { 1, 5, 6 }
-//         };
-//         int days = arr.length-1;
-//         int last = arr[0].length;
+public class Video_07_Ninja_Training {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 50, 1 },
+                { 5, 100, 11 },
+                { 1, 5, 6 }
+        };
+        int days = arr.length-1;
+        int last = arr[0].length;
 
-//         int ans = getMaxNinjaTrainingScore(arr, days, last);
-//         System.out.println(ans);
-//     }
-//     public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last){
-//         if(days == 0){
-//             int maxi = 0;
-//             for(int i=0; i<arr[0].length; i++){
-//                 if(i!=last){
-//                     maxi = Math.max(maxi, arr[0][i]);
-//                 }
-//             }
-//             return maxi;
-//         }
+        int ans = getMaxNinjaTrainingScore(arr, days, last);
+        System.out.println(ans);
+    }
+    public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last){
+        if(days == 0){
+            int maxi = 0;
+            for(int i=0; i<arr[0].length; i++){
+                if(i!=last){
+                    maxi = Math.max(maxi, arr[0][i]);
+                }
+            }
+            return maxi;
+        }
 
-//         int maxi = 0;
+        int maxi = 0;
 
-//         for(int i=0; i<arr[0].length; i++){
-//             if(i != last){
-//                 int point = arr[days][i] + getMaxNinjaTrainingScore(arr, days-1, i);
-//                 maxi = Math.max(maxi, point);
-//             }
-//         }
-//         return maxi;
-//     }
-// }
+        for(int i=0; i<arr[0].length; i++){
+            if(i != last){
+                int point = arr[days][i] + getMaxNinjaTrainingScore(arr, days-1, i);
+                maxi = Math.max(maxi, point);
+            }
+        }
+        return maxi;
+    }
+}
 
 //_________________________________________________________________________
 // Using memoization
-// import java.util.*;
-// public class Video_07_Ninja_Training {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//                 { 10, 50, 1 },
-//                 { 5, 100, 11 },
-//                 { 1, 5, 6 }
-//         };
-//         int days = arr.length-1;
-//         int last = arr[0].length;
+import java.util.*;
+public class Video_07_Ninja_Training {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 50, 1 },
+                { 5, 100, 11 },
+                { 1, 5, 6 }
+        };
+        int days = arr.length-1;
+        int last = arr[0].length;
 
-//         int[][] dp = new int[arr.length][arr[0].length+1];
-//         for(int[] temp: dp){
-//             Arrays.fill(temp,-1);
-//         }
+        int[][] dp = new int[arr.length][arr[0].length+1];
+        for(int[] temp: dp){
+            Arrays.fill(temp,-1);
+        }
 
-//         int ans = getMaxNinjaTrainingScore(arr, days, last, dp);
-//         System.out.println(ans);
-//     }
-//     public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last, int[][] dp){
-//         if(days == 0){
-//             int maxi = 0;
-//             for(int i=0; i<arr[0].length; i++){
-//                 if(i!=last){
-//                     maxi = Math.max(maxi, arr[0][i]);
-//                 }
-//             }
-//             return maxi;
-//         }
-//         if(dp[days][last] != -1){
-//             return dp[days][last];
-//         }
+        int ans = getMaxNinjaTrainingScore(arr, days, last, dp);
+        System.out.println(ans);
+    }
+    public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last, int[][] dp){
+        if(days == 0){
+            int maxi = 0;
+            for(int i=0; i<arr[0].length; i++){
+                if(i!=last){
+                    maxi = Math.max(maxi, arr[0][i]);
+                }
+            }
+            return maxi;
+        }
+        if(dp[days][last] != -1){
+            return dp[days][last];
+        }
 
-//         int maxi = 0;
+        int maxi = 0;
 
-//         for(int i=0; i<arr[0].length; i++){
-// if(i != last){
-//     int point = arr[days][i] + getMaxNinjaTrainingScore(arr, days-1, i,dp);
-//     maxi = Math.max(maxi, point);
-// }
-//         }
-//         return dp[days][last] = maxi;
-//     }
-// }
+        for(int i=0; i<arr[0].length; i++){
+if(i != last){
+    int point = arr[days][i] + getMaxNinjaTrainingScore(arr, days-1, i,dp);
+    maxi = Math.max(maxi, point);
+}
+        }
+        return dp[days][last] = maxi;
+    }
+}
 
 //_________________________________________________________________________
 // Using Tabulation
 
-// public class Video_07_Ninja_Training {
-//     public static void main(String[] args) {
-//         int[][] arr = {
-//                 { 10, 50, 1 },
-//                 { 5, 100, 11 },
-//                 { 1, 5, 6 }
-//         };
-//         int days = arr.length - 1;
-//         int last = arr[0].length;
+public class Video_07_Ninja_Training {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 50, 1 },
+                { 5, 100, 11 },
+                { 1, 5, 6 }
+        };
+        int days = arr.length - 1;
+        int last = arr[0].length;
 
-//         int ans = getMaxNinjaTrainingScore(arr, days, last);
-//         System.out.println(ans);
-//     }
+        int ans = getMaxNinjaTrainingScore(arr, days, last);
+        System.out.println(ans);
+    }
 
-//     public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last) {
-//         if (days == 0) {
-//             int maxi = 0;
-//             for (int i = 0; i < arr[0].length; i++) {
-//                 if (i != last) {
-//                     maxi = Math.max(maxi, arr[0][i]);
-//                 }
-//             }
-//             return maxi;
-//         }
+    public static int getMaxNinjaTrainingScore(int[][] arr, int days, int last) {
+        if (days == 0) {
+            int maxi = 0;
+            for (int i = 0; i < arr[0].length; i++) {
+                if (i != last) {
+                    maxi = Math.max(maxi, arr[0][i]);
+                }
+            }
+            return maxi;
+        }
 
-//         int[][] dp = new int[arr.length][arr[0].length + 1];
-//         dp[0][0] = Math.max(arr[0][1], arr[0][2]);
-//         dp[0][1] = Math.max(arr[0][0], arr[0][2]);
-//         dp[0][2] = Math.max(arr[0][0], arr[0][1]);
-//         dp[0][3] = Math.max(Math.max(arr[0][0], arr[0][1]), arr[0][2]);
+        int[][] dp = new int[arr.length][arr[0].length + 1];
+        dp[0][0] = Math.max(arr[0][1], arr[0][2]);
+        dp[0][1] = Math.max(arr[0][0], arr[0][2]);
+        dp[0][2] = Math.max(arr[0][0], arr[0][1]);
+        dp[0][3] = Math.max(Math.max(arr[0][0], arr[0][1]), arr[0][2]);
 
-//         for (int day = 1; day < arr.length; day++) {
-//             for (int i = 0; i < 4; i++) {
-//                 dp[day][i] = 0; 
-//                 for (int task = 0; task <= 2; task++) {
-//                     if (task != i) {
-//                         int activity = arr[day][task] + dp[day - 1][task];
-//                         dp[day][i] = Math.max(dp[day][i], activity);
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[arr.length - 1][arr[0].length];
-//     }
-// }
+        for (int day = 1; day < arr.length; day++) {
+            for (int i = 0; i < 4; i++) {
+                dp[day][i] = 0; 
+                for (int task = 0; task <= 2; task++) {
+                    if (task != i) {
+                        int activity = arr[day][task] + dp[day - 1][task];
+                        dp[day][i] = Math.max(dp[day][i], activity);
+                    }
+                }
+            }
+        }
+        return dp[arr.length - 1][arr[0].length];
+    }
+}
 
 //_________________________________________________________________________
 // Using Space Optimization
