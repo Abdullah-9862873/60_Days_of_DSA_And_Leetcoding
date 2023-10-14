8a0cb0eae6dd78b67b6b2d68e7b3a1d7179a957b
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-14 16:01:34 +0500

    Update Video_16_Partition_of_a_set_into_two_subsets.java

--- Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/03_DP on Subsequence,Subsets and Target/Video_16_Partition_of_a_set_into_two_subsets.java
+++ Day 23/Video Learnings/5-TakeUForward (Dynamic Programming)/03_DP on Subsequence,Subsets and Target/Video_16_Partition_of_a_set_into_two_subsets.java
@@ -1,3 +1,21 @@
+/*
+Problem Statement
+You are given an array 'arr' containing 'n' non- negative integers. Your task is to partition this array into two subsets such that the absolute difference between subset sums is minimum. You just need to find the minimum absolute difference considering any valid division of the array elements.
+
+Example:
+Input: 'n' = 5, 'arr' = [3, 1, 5, 2, 8].
+Ouput: 1
+Explanation: We can partition the given array into {3, 1, 5} and {2, 8}. This will give us the minimum possible absolute difference i.e. (10 - 9 = 1).
+
+Sample Input 1:
+4
+1 2 3 4
+
+Sample Output 1:
+0
+
+*/
+
 public class Video_16_Partition_of_a_set_into_two_subsets {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 6 };
