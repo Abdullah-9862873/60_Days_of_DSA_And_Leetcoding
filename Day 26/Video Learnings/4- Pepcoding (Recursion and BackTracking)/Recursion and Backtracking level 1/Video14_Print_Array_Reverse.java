70617bb1ad4e1cfa64df7df3cd419315976e190f
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-17 02:07:45 +0500

    Day 26

---
+++ Day 26/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 1/Video14_Print_Array_Reverse.java
@@ -0,0 +1,13 @@
+public class Video14_Print_Array_Reverse {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        printArray(arr, 0);
+    }
+    public static void printArray(int[] arr, int idx){
+        if(idx == arr.length){
+            return;
+        }
+        printArray(arr, idx+1);
+        System.out.println(arr[idx]);
+    }
+}
