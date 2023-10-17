af392f0eb7f9d21393bde2a7fc8e67da3c2c963f
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-17 22:41:38 +0500

    Day 28

--- Day 28/Video Learnings/5-TakeUForward (Dynamic Programming)/04_DP_on Strings/Video_32_Distinct_Subsequence_1D_Problem.java
+++ Day 28/Video Learnings/5-TakeUForward (Dynamic Programming)/04_DP_on Strings/Video_32_Distinct_Subsequence_1D_Problem.java
@@ -9,105 +9,132 @@
 // ________________________________________________________________________
 // Using Recursion
 
-public class Video_32_Distinct_Subsequence_1D_Problem{
-public static void main(String[] args) {
-String s1 = "babgbag";
-String s2 = "bag";
+public class Video_33_Edit_Distance {
+    public static void main(String[] args) {
+        String s1 = "horse";
+        String s2 = "ros";
 
-int ans = getAllSubsequenceOccurence(s1,s2, 0,0);
-System.out.println(ans);
-}
+        int ans = getMinimalEditDistance(s1, s2, 0, 0);
+        System.out.println(ans);
+    }
 
-public static int getAllSubsequenceOccurence(String s1, String s2, int i, int
-j){
-if(j==s2.length() || i == s1.length() && j == s2.length()){
-return 1;
-}
-if(i == s1.length() && j < s2.length()){
-return 0;
-}
+    public static int getMinimalEditDistance(String s1, String s2, int i, int j) {
+        if (i >= s1.length() && j < s2.length()) {
+            return s2.length() - j; 
+        } else if (j >= s2.length() && i < s1.length()) {
+            return s1.length() - i; 
+        } else if (i >= s1.length() && j >= s2.length()) {
+            return 0;
+        }
 
-int count = 0;
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return getMinimalEditDistance(s1, s2, i + 1, j + 1);
+        }
 
-if(s1.charAt(i) == s2.charAt(j)){
-count += getAllSubsequenceOccurence(s1, s2, i+1, j+1);
-}
-count += getAllSubsequenceOccurence(s1, s2, i+1, j);
-return count;
-}
+        int mini = Integer.MAX_VALUE;
+
+        // Insert Operation
+        int ans1 = 1 + getMinimalEditDistance(s1, s2, i, j + 1);
+
+        // Remove Operation
+        int ans2 = 1 + getMinimalEditDistance(s1, s2, i + 1, j);
+
+        // Replace Operation
+        int ans3 = 1 + getMinimalEditDistance(s1, s2, i + 1, j + 1);
+
+        mini = Math.min(ans1, Math.min(ans2, ans3));
+        return mini;
+    }
 }
 
 // _________________________________________________________________________
 // Using Memoization
 
-import java.util.*;
+import java.util.Arrays;
 
-public class Video_32_Distinct_Subsequence_1D_Problem {
+public class Video_33_Edit_Distance {
     public static void main(String[] args) {
-        String s1 = "babgbag";
-        String s2 = "bag";
+        String s1 = "horse";
+        String s2 = "ros";
 
-        int[][] dp = new int[s1.length()][s2.length()];
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
         for (int[] temp : dp) {
             Arrays.fill(temp, -1);
         }
-        int ans = getAllSubsequenceOccurence(s1, s2, 0, 0, dp);
+
+        int ans = getMinimalEditDistance(s1, s2, 0, 0, dp);
         System.out.println(ans);
     }
 
-    public static int getAllSubsequenceOccurence(String s1, String s2, int i, int j, int[][] dp) {
-        if (j == s2.length() || i == s1.length() && j == s2.length()) {
-            return 1;
+    public static int getMinimalEditDistance(String s1, String s2, int i, int j,
+            int[][] dp) {
+        if (i == s1.length()) {
+            return s2.length() - j;
+        } else if (j == s2.length()) {
+            return s1.length() - i;
         }
-        if (i == s1.length() && j < s2.length()) {
-            return 0;
-        }
+
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int count = 0;
-
         if (s1.charAt(i) == s2.charAt(j)) {
-            count += getAllSubsequenceOccurence(s1, s2, i + 1, j + 1, dp);
+            return dp[i][j] = getMinimalEditDistance(s1, s2, i + 1, j + 1, dp);
         }
-        count += getAllSubsequenceOccurence(s1, s2, i + 1, j, dp);
-        return dp[i][j] = count;
+
+        int insert = 1 + getMinimalEditDistance(s1, s2, i, j + 1, dp);
+        int remove = 1 + getMinimalEditDistance(s1, s2, i + 1, j, dp);
+        int replace = 1 + getMinimalEditDistance(s1, s2, i + 1, j + 1, dp);
+
+        dp[i][j] = Math.min(Math.min(insert, remove), replace);
+        return dp[i][j];
     }
 }
 
 // _________________________________________________________________________
 // Using Tabulation
 
-public class Video_32_Distinct_Subsequence_1D_Problem {
+public class Video_33_Edit_Distance {
     public static void main(String[] args) {
-        String s1 = "babgbag";
-        String s2 = "bag";
+        String s1 = "horse";
+        String s2 = "ros";
 
-        int ans = getAllSubsequenceOccurence(s1, s2, 0, 0);
+        int ans = getMinimalEditDistance(s1, s2);
         System.out.println(ans);
     }
 
-    public static int getAllSubsequenceOccurence(String s1, String s2, int i, int j) {
+    public static int getMinimalEditDistance(String s1, String s2) {
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
         // Base Cases
-        for (int k = 0; k <= s1.length(); k++) {
-            dp[k][s2.length()] = 1;
+        for (int j = 0; j <= s2.length(); j++) {
+            dp[s1.length()][j] = s2.length() - j;
         }
-        dp[s1.length()][s2.length()] = 1;
 
-        for (int k = s1.length() - 1; k >= 0; k--) {
-            for (int l = s2.length() - 1; l >= 0; l--) {
-                int count = 0;
-                if (k == s1.length() && l < s2.length()) {
-                    dp[k][l] = 0;
-                } else if (s1.charAt(k) == s2.charAt(l)) {
-                    count += dp[k + 1][l + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            dp[i][s2.length()] = s1.length() - i;
+        }
+
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    int mini = Integer.MAX_VALUE;
+
+                    // Insert Operation
+                    int ans1 = 1 + dp[i][j + 1];
+
+                    // Remove Operation
+                    int ans2 = 1 + dp[i + 1][j];
+
+                    // Replace Operation
+                    int ans3 = 1 + dp[i + 1][j + 1];
+
+                    mini = Math.min(ans1, Math.min(ans2, ans3));
+                    dp[i][j] = mini;
                 }
-                count += dp[k + 1][l];
-
-                dp[k][l] = count;
             }
         }
         return dp[0][0];
@@ -117,35 +144,46 @@ public class Video_32_Distinct_Subsequence_1D_Problem {
 // _________________________________________________________________________
 // Using Space optimization
 
-public class Video_32_Distinct_Subsequence_1D_Problem {
+public class Video_33_Edit_Distance {
     public static void main(String[] args) {
-        String s1 = "babgbag";
-        String s2 = "bag";
+        String s1 = "horse";
+        String s2 = "ros";
 
-        int ans = getAllSubsequenceOccurence(s1, s2, 0, 0);
+        int ans = getMinimalEditDistance(s1, s2);
         System.out.println(ans);
     }
 
-    public static int getAllSubsequenceOccurence(String s1, String s2, int i, int j) {
-        int[] front = new int[s1.length() + 1];
-        int[] curr = new int[s1.length() + 1];
-
+    public static int getMinimalEditDistance(String s1, String s2) {
+        int[] front = new int[s2.length() + 1];
+        int[] curr = new int[s2.length() + 1];
+    
         // Base Cases
-        curr[s2.length()] = 1;
-        front[s2.length()] = 1;
-
-        for (int k = s1.length() - 1; k >= 0; k--) {
-            for (int l = s2.length() - 1; l >= 0; l--) {
-                int count = 0;
-                if (k == s1.length() && l < s2.length()) {
-                    curr[l] = 0;
-                } else if (s1.charAt(k) == s2.charAt(l)) {
-                    count += front[l + 1];
+        for (int j = 0; j <= s2.length(); j++) {
+            front[j] = s2.length() - j;
+        }
+        
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            curr[s2.length()] = s1.length() - i;
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    curr[j] = front[j + 1];
+                } else {
+                    int mini = Integer.MAX_VALUE;
+    
+                    // Insert Operation
+                    int ans1 = 1 + curr[j + 1];
+    
+                    // Remove Operation
+                    int ans2 = 1 + front[j];
+    
+                    // Replace Operation
+                    int ans3 = 1 + front[j + 1];
+    
+                    mini = Math.min(ans1, Math.min(ans2, ans3));
+                    curr[j] = mini;
                 }
-                count += front[l];
-
-                curr[l] = count;
             }
+    
             int[] temp = front;
             front = curr;
             curr = temp;
