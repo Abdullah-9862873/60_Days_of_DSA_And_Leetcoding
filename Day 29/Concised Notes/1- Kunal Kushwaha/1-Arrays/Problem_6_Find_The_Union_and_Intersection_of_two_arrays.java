94b932d158841e5a08140f5fe23d6aca9c080566
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-19 00:55:17 +0500

    Day 29

---
+++ Day 29/Concised Notes/1- Kunal Kushwaha/1-Arrays/Problem_6_Find_The_Union_and_Intersection_of_two_arrays.java
@@ -0,0 +1,23 @@
+/*
+Wherever you have to deal with unique elements... Consider "Sets"
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem_6_Find_The_Union_and_Intersection_of_two_arrays {
+    public static void main(String[] args) {
+        int[] a = { 85, 9, 6, 99, 12 };
+        int[] b = { 1, 12, 6 };
+        int n = a.length;
+        int m = b.length;
+
+        Set<Integer> mySet = new HashSet<>();
+        for(int i=0; i<n; i++){
+            mySet.add(a[i]);
+        }
+        for(int i=0; i<m; i++){
+            mySet.add(b[i]);
+        }
+        System.out.println(mySet.size());
+    }
+}
