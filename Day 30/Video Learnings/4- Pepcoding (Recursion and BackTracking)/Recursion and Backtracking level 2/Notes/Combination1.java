7f741fc49c7b77100b4df2f336d961570a8a2641
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-10-19 04:53:03 +0500

    Day 30

---
+++ Day 30/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 2/Notes/Combination1.java
@@ -0,0 +1,27 @@
+/*
+Question... Write all the combinations to place 1 item in 4 boxes
+Boxes on levels in this example
+ */
+
+public class Combination1 {
+    public static void main(String[] args) {
+        int currentBox = 0;
+        int totalBoxes = 4;
+        int selectionSoFar = 0;
+        int totalSelection = 1;
+        String answerSoFar = "";
+        printCombination(currentBox, totalBoxes, selectionSoFar, totalSelection, answerSoFar);
+    }
+    public static void printCombination(int cb, int tb, int ssf, int ts, String asf) {
+        if (cb >= tb) {
+            if (ssf == ts) {
+                System.out.println(asf);
+            }
+            return;
+        }
+
+        printCombination(cb + 1, tb, ssf + 1, ts, asf + "1"); // Yes Call
+        printCombination(cb + 1, tb, ssf, ts, asf + "-"); // No Call
+
+    }
+}
