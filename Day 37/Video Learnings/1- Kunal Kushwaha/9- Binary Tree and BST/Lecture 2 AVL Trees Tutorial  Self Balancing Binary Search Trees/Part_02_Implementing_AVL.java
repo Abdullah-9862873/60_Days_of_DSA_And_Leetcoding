363c18e06bee1bbc706de90b187f15fc5f7f557c
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-11-11 01:36:16 +0500

    Day 37

--- Day 37/Video Learnings/1- Kunal Kushwaha/9- Binary Tree and BST/Lecture 2 AVL Trees Tutorial  Self Balancing Binary Search Trees/Part_02_Implementing_AVL.java
+++ Day 37/Video Learnings/1- Kunal Kushwaha/9- Binary Tree and BST/Lecture 2 AVL Trees Tutorial  Self Balancing Binary Search Trees/Part_02_Implementing_AVL.java
@@ -1,3 +1,4 @@
+
 /*
 Initial tree
 
@@ -19,8 +20,9 @@ You can obtain the exact first tree after performing leftRotation(p) on the seco
 */
 import java.util.Scanner;
 
-public class Part_02_Implementing_AVL{
+public class Part_02_Implementing_AVL {
     public static Node root;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the root element: ");
@@ -30,36 +32,37 @@ public class Part_02_Implementing_AVL{
         display(root, "", false);
 
         boolean balanced = checkBalance(root);
-        if(balanced){
+        if (balanced) {
             System.out.println("The binary search tree is balanced");
-        }else{
+        } else {
             System.out.println("The binary search tree is not balanced");
         }
-    } 
+    }
+
     // Identifying the four cases
-    public static Node rotate(Node node){
-        // Left Heavy Case
-        // Here node is representing "p" and we are observing "c"
-        if(height(node.left) - height(node.right) > 1){
-            // It is left heavy... It will either be left-left or left-right
-            // Now to determine whether it is left-left or left-right we have to observe the behavior of "g"
-            if(height(node.left.left) - height(node.left.right) > 0){
-                // Left left case
+    public static Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
                 return rightRotate(node);
             }
-            else if(height(node.left.left) - height(node.left.right) < 0){
+            if (height(node.left.left) - height(node.left.right) < 0) {
                 // left right case
                 node.left = leftRotate(node.left);
                 return rightRotate(node);
             }
-        }else if(height(node.left) - height(node.right) < -1){
-            // Right Heavy
-            if(height(node.right.left) - height(node.right.right) > 0){
-                // Right Left Case
-                node.right = rightRotate(node.right);
+        }
+
+        if (height(node.left) - height(node.right) < -1) {
+            // right heavy
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
                 return leftRotate(node);
-            }else if(height(node.right.left) - height(node.right.right) < 0){
-                // Right Right Case
+            }
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
+                node.right = rightRotate(node.right);
                 return leftRotate(node);
             }
         }
@@ -67,8 +70,8 @@ public class Part_02_Implementing_AVL{
         return node;
     }
 
-    public static Node rightRotate(Node p){
-        // During Right Rotation we have to change the position of "c" "t2" and "p" 
+    public static Node rightRotate(Node p) {
+        // During Right Rotation we have to change the position of "c" "t2" and "p"
         // Look at the diagram at the top
         Node c = p.left;
         Node t2 = c.right;
@@ -82,7 +85,7 @@ public class Part_02_Implementing_AVL{
         return c;
     }
 
-    public static Node leftRotate(Node c){
+    public static Node leftRotate(Node c) {
         // During the left Rotation we have to change the position of "c", "t2"
         Node p = c.right;
         Node t2 = p.left;
@@ -94,23 +97,23 @@ public class Part_02_Implementing_AVL{
         return p;
     }
 
-    public static void populate(Scanner input){
+    public static void populate(Scanner input) {
         System.out.println("Do you want to insert a value: (yes/no)");
         String userChoice = input.next();
-        if(userChoice.equalsIgnoreCase("yes")){
+        if (userChoice.equalsIgnoreCase("yes")) {
             System.out.println("Enter the value: ");
             int value = input.nextInt();
             Node newNode = new Node(value);
-            insert(root, newNode);
+            root = insert(root, newNode);
             populate(input);
         }
     }
 
     public static Node insert(Node root, Node node) {
         if (root == null) {
-            return node;  
+            return node;
         }
-    
+
         if (node.value < root.value) {
             root.left = insert(root.left, node);
         } else if (node.value > root.value) {
@@ -121,10 +124,9 @@ public class Part_02_Implementing_AVL{
 
         return rotate(root);
     }
-    
 
-    public static void display(Node node, String str, boolean isLeft){
-        if(node == null){
+    public static void display(Node node, String str, boolean isLeft) {
+        if (node == null) {
             return;
         }
         System.out.println(str + (isLeft ? "├── " : "└── ") + node.value);
@@ -132,30 +134,28 @@ public class Part_02_Implementing_AVL{
         display(node.right, str + (isLeft ? "|   " : "    "), false);
     }
 
-    public static boolean checkBalance(Node node){
-        if(node == null){
+    public static boolean checkBalance(Node node) {
+        if (node == null) {
             return true;
         }
 
-        int leftHeight = (node.left == null) ? -1 : node.left.height;
-        int rightHeight = (node.right == null) ? -1 : node.right.height;
-        return Math.abs(leftHeight - rightHeight) <= 1 && checkBalance(node.left) && checkBalance(node.right);
+        return height(node.left) - height(node.right) <= 1 && checkBalance(node.left) && checkBalance(node.right);
     }
 
-    public static int height(Node node){
-        if(node == null){
+    public static int height(Node node) {
+        if (node == null) {
             return -1;
         }
         return node.height;
     }
 
-    public static class Node{
+    public static class Node {
         int value;
         Node left;
         Node right;
         private int height;
 
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
         }
     }
