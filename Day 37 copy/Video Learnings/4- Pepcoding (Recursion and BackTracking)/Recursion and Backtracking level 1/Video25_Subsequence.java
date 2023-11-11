856c7d36cf8de10a31c3aa5b4378754456b9ee27
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-11-12 01:08:40 +0500

    Day 38

---
+++ Day 37 copy/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 1/Video25_Subsequence.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Video25_Subsequence {
+    public static void main(String[] args) {
+        System.out.println(printSubSequenceUsingRecursion("abc"));
+    }
+    public static ArrayList<String> printSubSequenceUsingRecursion(String str){
+        if(str.length() == 0){
+            ArrayList<String> ans = new ArrayList<>();
+            ans.add("");
+            return ans;
+        }
+
+        char ch = str.charAt(0);
+        String subStr = str.substring(1);
+        ArrayList<String> recResponse = printSubSequenceUsingRecursion(subStr);
+
+        ArrayList<String> myResponse = new ArrayList<>();
+        for(String temp : recResponse){
+            myResponse.add("" + temp);
+            myResponse.add(ch + temp);
+        }
+        return myResponse;
+    }
+    // Using Power Set
+    public static void printSubsequence(String str){
+        for(int i=0; i<Math.pow(2,str.length()); i++){
+            StringBuilder ans = new StringBuilder();
+            for(int j=0; j<str.length(); j++){
+                if((i & (1 << j)) != 0){
+                    ans.append(str.charAt(j));
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
---
+++ Day 37 copy/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 2/Lectures/Video11_Parition_in_K_subsets.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Video11_Parition_in_K_subsets {
+    public static void main(String[] args) {
+        int n = 6;
+        int k = 3;
+        if (n < k) {
+            System.out.println("Not Possible");
+        } else if (k == 1) {
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                ans = ans * 10 + i;
+            }
+            System.out.println(ans);
+        } else if (n == k) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= n; i++) {
+                sb.append("(").append(i).append(")");
+            }
+            System.out.println(sb.toString());
+        } else {
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            for(int i=0; i<k; i++){
+                ans.add(new ArrayList<>());
+            }
+            solve(1, n, k, 0, ans);
+        }
+    }
+
+    public static void solve(int i, int n, int k, int noOfSets, ArrayList<ArrayList<Integer>> ans) {
+        if(i>n){
+            if(noOfSets == k){
+                for(ArrayList<Integer> set : ans){
+                    System.out.print(set + ",");
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for(int j=0; j<ans.size(); j++){
+            if(ans.get(j).size() > 0){
+                // If there is already an answer present then simply add my number
+                ans.get(j).add(i);
+                solve(i+1, n, k, noOfSets, ans);
+                ans.get(j).remove(ans.get(j).size()-1);
+            }else{
+                // If there is nothing then simply add number is the empty set
+                ans.get(j).add(i);
+                solve(i+1, n, k, noOfSets+1, ans);
+                ans.get(j).remove(ans.get(j).size()-1);
+                break;
+            }
+        }
+    }
+
+}
