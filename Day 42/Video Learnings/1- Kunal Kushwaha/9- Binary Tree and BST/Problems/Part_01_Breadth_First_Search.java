f6dc3d566e78b394cf6741b99cd95c6f5335a4f9
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-11-16 05:11:23 +0500

    Day 42

---
+++ Day 42/Video Learnings/1- Kunal Kushwaha/9- Binary Tree and BST/Problems/Part_01_Breadth_First_Search.java
@@ -0,0 +1,17 @@
+/*
+-----------------When to Use??-----------------
+----> When answer lies near the root node
+----> When you are asked to search by level
+
+-----------------Example-----------------
+----> Calculate the sum of nodes in binary search tree of each level
+
+                                        1
+                                       /  \
+                                      2    3
+                                     / \  / \
+                                    4  5  6  7
+
+BFS will be like 1-2-3-4-5-6-7
+
+*/
