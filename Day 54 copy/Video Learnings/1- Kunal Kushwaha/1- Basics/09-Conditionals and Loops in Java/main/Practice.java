26b4b77750818ba2c61fc5c0bf3dbeecb6271397
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2024-01-21 00:32:26 +0500

    Delete Day 54 copy directory

--- Day 54 copy/Video Learnings/1- Kunal Kushwaha/1- Basics/09-Conditionals and Loops in Java/main/Practice.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Practice {
-    public static void main(String[] args) {
-        String str = " abd ";
-        str = str.trim();
-        System.out.println(str.charAt(str.length()-1));
-    }
-}
