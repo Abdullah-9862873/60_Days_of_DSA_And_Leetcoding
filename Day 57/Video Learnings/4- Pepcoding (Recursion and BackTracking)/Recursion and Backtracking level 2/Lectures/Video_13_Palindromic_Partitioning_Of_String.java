a8c91acff32b8ca17b9e482ffab437b72f52e398
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2024-01-22 01:24:43 +0500

    Day 57

---
+++ Day 57/Video Learnings/4- Pepcoding (Recursion and BackTracking)/Recursion and Backtracking level 2/Lectures/Video_13_Palindromic_Partitioning_Of_String.java
@@ -0,0 +1,35 @@
+public class Video_13_Palindromic_Partitioning_Of_String {
+    public static void main(String[] args) {
+        String str = "abaaba";
+        printPalindromicPartitions(str, "");
+    }
+    public static void printPalindromicPartitions(String str, String asf){
+        if(str.length() == 0){
+            System.out.println(asf);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            String myString = str.substring(0, i+1);
+            String ros = str.substring(i+1);
+            if(isPalindromic(myString)){
+                printPalindromicPartitions(ros, asf + "(" + myString + ") ");
+            }
+        }
+    }
+    public static boolean isPalindromic(String str){
+        int start = 0;
+        int end = str.length()-1;
+
+        while(start < end){
+            if(str.charAt(start) == str.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Every partition should itself be palindromic
